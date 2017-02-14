ff994a9325f048aa6febd98b0499cb866b582ebd
asdyking <asdyking@me.com>
2017-02-14 17:03:32 +0800

    Demo测试工程

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ComDelegateTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.run_btn = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.com_in_text = new System.Windows.Forms.TextBox();
+            this.com_out_text = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // run_btn
+            // 
+            this.run_btn.Location = new System.Drawing.Point(403, 16);
+            this.run_btn.Name = "run_btn";
+            this.run_btn.Size = new System.Drawing.Size(102, 38);
+            this.run_btn.TabIndex = 0;
+            this.run_btn.Text = "启动";
+            this.run_btn.UseVisualStyleBackColor = true;
+            this.run_btn.Click += new System.EventHandler(this.run_btn_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 71);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(478, 358);
+            this.textBox1.TabIndex = 2;
+            // 
+            // com_in_text
+            // 
+            this.com_in_text.Location = new System.Drawing.Point(61, 25);
+            this.com_in_text.Name = "com_in_text";
+            this.com_in_text.Size = new System.Drawing.Size(100, 25);
+            this.com_in_text.TabIndex = 3;
+            // 
+            // com_out_text
+            // 
+            this.com_out_text.Location = new System.Drawing.Point(260, 25);
+            this.com_out_text.Name = "com_out_text";
+            this.com_out_text.Size = new System.Drawing.Size(100, 25);
+            this.com_out_text.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 15);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "COM";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(223, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "COM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 453);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.com_out_text);
+            this.Controls.Add(this.com_in_text);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.run_btn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button run_btn;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox com_in_text;
+        private System.Windows.Forms.TextBox com_out_text;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace ComDelegateTest
+{
+    public partial class Form1 : Form
+    {
+        //声明
+        public delegate void ComRecvCall(String buf,int size);
+        //设置回调函数
+        [DllImport("ComDelegate.dll")]
+        public static extern bool CommSetCallback(ComRecvCall fa);
+        [DllImport("ComDelegate.dll")]
+        public static extern String version();
+        [DllImport("ComDelegate.dll")]
+        public static extern int activate(String sign);
+        [DllImport("ComDelegate.dll")]
+        public static extern int setComIn(String com_in);
+        [DllImport("ComDelegate.dll")]
+        public static extern int setComOut(String com_out);
+        [DllImport("ComDelegate.dll")]
+        public static extern int initDelegate();
+        [DllImport("ComDelegate.dll")]
+        public static extern int uninitDelegate();
+
+        //声明回调的函数
+        public void ComRecvCallImp(String buf, int size)
+        {
+            MessageBox.Show(buf);
+            return;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            //调用
+            CommSetCallback(ComRecvCallImp);
+        }
+
+        private void run_btn_Click(object sender, EventArgs e)
+        {
+            if (run_btn.Text == "启动")
+            {
+                String com_port = com_in_text.Text;
+                com_port = com_port + "&9600&N&8&1";
+                setComIn(com_port);
+
+                com_port = com_out_text.Text;
+                com_port = com_port + "&9600&N&8&1";
+                setComOut(com_port);
+
+                initDelegate();
+
+                run_btn.Text = "停止";
+            }
+            else
+            {
+                run_btn.Text = "启动";
+                uninitDelegate();
+            }
+        }
+    }
+}
